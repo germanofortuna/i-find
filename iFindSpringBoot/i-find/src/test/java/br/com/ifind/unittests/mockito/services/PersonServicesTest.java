e3706ef2df37cd40438dc3c7533630b70defdd14
Germano Fortuna <gfortuna@outlook.com>
2023-01-20 09:40:55 -0300

    add hateoas test

--- iFindSpringBoot/i-find/src/test/java/br/com/ifind/unittests/mockito/services/PersonServicesTest.java
+++ iFindSpringBoot/i-find/src/test/java/br/com/ifind/unittests/mockito/services/PersonServicesTest.java
@@ -1,9 +1,11 @@
  package br.com.ifind.unittests.mockito.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -40,40 +42,8 @@ class PersonServicesTest {
 		input = new MockPerson();
 		MockitoAnnotations.openMocks(this);
 	}
-
-	/*
-	@Test
-	void testFindAll() throws Exception {
-		List<Person> list = input.mockEntityList();
-		
-		when(repository.findAll()).thenReturn(list);
-		
-		var people = service.findAll();
-		assertNotNull(people);
-		assertEquals(14, people.size());
-		
-		var personOne = people.get(1);
-		assertTrue(personOne.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
-		assertEquals("Addres Test1",personOne.getAddress());
-		assertEquals("First Name Test1",personOne.getFirstName());
-		assertEquals("Last Name Test1",personOne.getLastName());
-		assertEquals("Female",personOne.getGender());
-		
-		var personFour = people.get(4);
-		assertTrue(personFour.toString().contains("links: [</api/person/v1/4>;rel=\"self\"]"));
-		assertEquals("Addres Test4",personFour.getAddress());
-		assertEquals("First Name Test4",personFour.getFirstName());
-		assertEquals("Last Name Test4",personFour.getLastName());
-		assertEquals("Male",personFour.getGender());
-		
-		var personSeven = people.get(7);
-		assertTrue(personSeven.toString().contains("links: [</api/person/v1/7>;rel=\"self\"]"));
-		assertEquals("Addres Test7",personSeven.getAddress());
-		assertEquals("First Name Test7",personSeven.getFirstName());
-		assertEquals("Last Name Test7",personSeven.getLastName());
-		assertEquals("Female",personSeven.getGender());
-	}
-	*/
+	
+	
 	@Test
 	void testFindById() throws Exception {
 		Person entity = input.mockEntity(1);
